5e3ae2f9c18d07038bb5c97a9ef2e7387e8c5757
Atharv Pathak <78468172+Atharv-Pathak-14@users.noreply.github.com>
2022-08-24 16:55:17 +0530

    Update hero.js

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -58,9 +58,9 @@ const Hero = () => {
     return () => clearTimeout(timeout);
   }, []);
 
-  const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const one = <h1>Hello, I am</h1>;
+  const two = <h2 className="big-heading">Omniverse.</h2>;
+  const three = <h3 className="big-heading">A epic music </h3>;
   const four = (
     <>
       <p>
